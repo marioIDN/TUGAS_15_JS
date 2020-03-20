fa0291fcfc437c3f7d414b12dea558b5e377d10b
Mardionocs <mardionoputra19>
2020-03-21 02:29:59 +0700

    TUGAS 15 JS
    Tugas dari Niomic

---
+++ tugas15.js
@@ -0,0 +1,14 @@
+function tugas() {
+  var ab = 8;
+  var bc = 6;
+
+  console.log("Nilai AB :",ab);
+  console.log("Nilai BC :",bc);
+
+  var x = Math.pow(ab,2);
+  var y = Math.pow(bc,2);
+
+  var z = x + y;
+  console.log("panjang sisi AC pada segitiga siku-siku tersebut adalah :",Math.sqrt(z));
+}
+tugas()
